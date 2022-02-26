8df3773720bf9c981b13baead6a6662a38d5d049
Elisha Delight <77095312+PowertechDevelopment@users.noreply.github.com>
2022-02-26 13:18:36 +0100

    Update Fruitask.java

--- Fruitask.java
+++ Fruitask.java
@@ -1,3 +1,17 @@
+/**  ~~~~~
+ * Created with the AppyBuilder Code Editor.
+ * This is a template for basic Extension.
+ * Modify this template to customize your extension.
+ *
+ * **** NOTE: DO NOT use a package name. 
+ * **** The package name will be created for you automatically.
+ * **** Adding a package name will cause a compile error
+ */
+import android.content.Context;
+import android.util.Log;
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.runtime.*;
+import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.annotations.DesignerProperty;
 import com.google.appinventor.components.annotations.SimpleEvent;
 import com.google.appinventor.components.annotations.SimpleFunction;
@@ -8,9 +22,10 @@ import com.google.appinventor.components.runtime.EventDispatcher;
 import com.google.appinventor.components.runtime.util.YailList;
 import com.google.appinventor.components.runtime.errors.YailRuntimeError;
 
-@DesignerComponent(version = 1,  description = "Fruitask Extenasion v1.0.<br>",
+@DesignerComponent(version = 1,  description = "This Extension was created with the AppyBuilder Code Editor.<br>" + 
+                   "Create your own here:<br><a href='https://editor.appybuilder.com' target='_blank'>https://editor.appybuilder.com</a><br>",
         category = ComponentCategory.EXTENSION,
-        nonVisible = true,   iconName = "https://fruitask.com/assets/logo/Fruitask-logo.svg")
+        nonVisible = true,   iconName = "http://appyBuilder.com/extensions/icons/extension.png")
 @SimpleObject(external = true)
 public class Fruitask extends AndroidNonvisibleComponent {
     private ComponentContainer container;
@@ -21,7 +36,8 @@ public class Fruitask extends AndroidNonvisibleComponent {
         super(container.$form());
         this.container = container;
     }
-  private String Apikey;
+  
+   private String Apikey;
   private String Baseid;
   private String Table_Name;
   private int Rowid;
@@ -70,16 +86,16 @@ public class Fruitask extends AndroidNonvisibleComponent {
     Values = Values;
   }
   @SimpleEvent
-  public void Row Created(int response_Code){
-    EventDispatcher.dispatchEvent(this,"Row Created",response_Code);
+  public void Row_Created(int response_Code){
+    EventDispatcher.dispatchEvent(this,"Row_Created",response_Code);
   }
   @SimpleFunction
   public void Delete_Row(int row_Number){
     Rowid = row_Number;
   }
   @SimpleEvent
-  public void Row Deleted(int response_Code){
-    EventDispatcher.dispatchEvent(this,"Row Deleted", response_Code);
+  public void Row_Deleted(int response_Code){
+    EventDispatcher.dispatchEvent(this,"Row_Deleted",response_Code);
   }
   @SimpleFunction
   public void Get_Cell(int row_Number,String column_Name){
@@ -87,8 +103,8 @@ public class Fruitask extends AndroidNonvisibleComponent {
     column_Name = column_Name;
   }
   @SimpleEvent
-  public void Got Cell(int response_Code,String value, int row id,String created_Time){
-   EventDispatcher.dispatchEvent(this,"Got Cell",response_Code,value,row id,created_Time);
+  public void Got_Cell(int response_Code,String value,int row_id,String created_Time){
+   EventDispatcher.dispatchEvent(this,"Got_Cell",response_Code,value,row_id,created_Time);
   }
   @SimpleFunction
   public void Get_Column(String column_Name, String max_Record){
@@ -111,8 +127,8 @@ public class Fruitask extends AndroidNonvisibleComponent {
     updatename = column_Names;
     updatevalue = values;
   }
-  @SimpleFunction
+ @SimpleFunction
   public void Get_All_Rows(){
- 
+    
   }
 }
